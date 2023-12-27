c2821ff6f0f19ac6ae25834e3d29d9c706ad8495
EmilioJasso19 <emiliojasso0409@gmail.com>
2023-12-27 14:18:33 -0600

    Intento 2

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const session = require('express-session');
 const crypto = require('crypto');
-const compression = require('compression');
-app.use(compression());
 
 // crear instancia Express
 const app = express();
