8d9b52e967e6315e2e63cb4c7f67d4ac222aa46d
EmilioJasso19 <emiliojasso0409@gmail.com>
2023-12-27 21:03:00 -0600

    prueba 5 para intento 7

--- public/js/script.js
+++ public/js/script.js
@@ -151,8 +151,8 @@ function loadAppointments(page, limit) {
   .then(response => response.json())
   .then(data => {
     console.log('esto es data', data);
-      const appointments = data[0][0];
-      console.log('lo que deberia ser appointments', data[0])
+      const appointments = data;
+      console.log('lo que deberia ser appointments', data)
       const totalPages = data[0][1];
       console.log('totalpages:', totalPages);
       const totalAppointments = data[0][2];
